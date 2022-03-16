9fb8353e259d4062a90550c510254abe7bf1c5ec
lesenelir <miaomiaobabyzy@gmail.com>
2022-03-16 15:32:38 +0800

    feature: modify element plus icon vue3 problem

--- src/main.js
+++ src/main.js
@@ -4,5 +4,12 @@ import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as Icons from '@element-plus/icons'
 
-createApp(App).use(ElementPlus).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+Object.keys(Icons).forEach(key => {
+  app.component(key, Icons[key])
+})
+
+app.use(ElementPlus).use(store).use(router).mount('#app')
